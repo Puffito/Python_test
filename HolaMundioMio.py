f0f1e52f2cbf1fbe2a41b5c94045340e872f48c2
Puffito <puffitored@gmail.com>
2023-01-23 17:04:11 +0100

    Hola Python

---
+++ HolaMundioMio.py
@@ -0,0 +1,6 @@
+""" frase = input("Introduce una frase para repetir")
+veces = int(input("Introduce el número de veces que quieres que se repita"))
+for i in range(veces):
+    print(frase) """
+    
+print("Hola buenas {} pase usted a {}".format("Luisa",34))
